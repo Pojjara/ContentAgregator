80e868d58a869d2ccd819ba1d4769c10194ce9b2
Pojjara <jacekmalarewicz@gmail.com>
2023-01-04 21:39:59 +0000

    sdsd 	new file:   articles.xlsx

--- ListOfSites.py
+++ ListOfSites.py
@@ -1,14 +1,14 @@
 sites = [
     {"name" : "FC Barca",
-    "logo" : "https://www.fcbarca.com/static/gfx/fcbarcacomlogo_new6.svg",
+    "logo" : "https://www.fcbarca.com/static/gfx/FCBarcacomSwietaKorekta.svg",
     "link" : "http://fcbarca.com",
     "id" : 1},
     {"name" : "BBC",
     "logo" : "https://logos-world.net/wp-content/uploads/2022/01/BBC-Logo.png",
-    "link" : "http://BBC.co.uk",
+    "link" : "https://www.bbc.co.uk/news/world",
     "id" : 2},
     {"name": "Dziennik Naukowy",
     "logo" : "https://dzienniknaukowy.pl/themes/flatly/img/logo.png",
-    "link": "https://dzienniknaukowy.pl/",
+    "link": "https://dzienniknaukowy.pl/aktualnosci/artykuly?orderBy=publication_date&sortOrder=desc",
     "id" : 3}
 ]
--- functions.py
+++ functions.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, redirect, request, session, g
 import requests, bs4
 from flask_mysqldb import MySQL
 import sqlite3
+import csv
+import openpyxl
 
 def initializeDB():
     conn = sqlite3.connect('database.db')
@@ -31,6 +33,17 @@ def initializeDB():
 
     conn.close()
 
+# Write a header row to the CSV file
+with open('articles.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Title', 'Body', 'Link', 'Site ID'])
+
+# Write a header row to the Excel file
+wb = openpyxl.Workbook()
+sheet = wb.active
+sheet.append(['Title', 'Body', 'Link', 'Site ID'])
+wb.save('articles.xlsx')
+    
 def addSitesToDB(sites):
     for site in sites:
         siteName = site["name"]
@@ -50,33 +63,68 @@ def getArticlesFromSites(sites):
         siteName = site["name"]
         link = site['link']
 
-
         if site["name"] == "FC Barca":    
 
+            articles_list = soup.select('.news__list')[0]
             for i in range(5):
-                articles_list = soup.select('.news__list')[0]
-
-
+                
                 article = articles_list.find_all(class_='article')[i] # Finds all articles
-
                 article_title = article.select('h3.article__meta__title')[0].text # Gets titles from articles
-
                 article_body = article.select('div.article__meta__content')[0].text
-
                 site_id = site["id"]
-                #print(article_content)
-
                 article_link = site['link'] + article.find('a')['href'] # Gets links from articles
 
-                connection = sqlite3.connect("database.db")
-                cursor = connection.cursor()   
-                cursor.execute('INSERT INTO articles(article_title,article_body,article_link,site_ID) VALUES(?,?,?,?)', (article_title,article_body,article_link,site_id))
-                connection.commit()
-                connection.close()
+                insertIntoDB(article_title,article_body,article_link,site_id)
                 
         elif site['name'] == "BBC":
-            continue
 
+            # Find all div elements with class "gs-c-promo-body"
+            articles = soup.find_all('div', class_='gs-c-promo-body')
+            xOfArticles = 5
+            for article in articles[:xOfArticles]:
+                try:
+                    article_title = article.find('h3').text
+                    article_body = article.find('p').text
+                    article_link = 'https://bbc.co.uk' + article.find('a')['href']
+                    site_id = site['id']
+                except:
+                    xOfArticles =+1
+                    continue
+
+                insertIntoDB(article_title,article_body,article_link,site_id)
+
+        elif site['name'] == "Dziennik Naukowy":
+
+
+            # Find the articles
+            articles = soup.find_all("div", class_="article-list")
+            site_id = site['id']
+            # Print the titles and contents of the 5 most recent articles
+            for article in articles[:5]:
+                article_title = article.find(class_='title').text
+                article_body = article.find(class_='contents').text
+                article_link = article.find(class_='read-more')['href']
+                insertIntoDB(article_title,article_body[40:-141],article_link,site_id)
+                
+        
+
+def insertIntoDB(article_title,article_body,article_link,site_id):
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()   
+    cursor.execute('INSERT INTO articles(article_title,article_body,article_link,site_ID) VALUES(?,?,?,?)', (article_title,article_body,article_link,site_id))
+    connection.commit()
+    connection.close()
+
+    # Write the information to a CSV file
+    with open('articles.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([article_title, article_body, article_link, site_id])
+
+    # Write the information to an Excel file
+    wb = openpyxl.load_workbook('articles.xlsx')
+    sheet = wb.active
+    sheet.append([article_title, article_body, article_link, site_id])
+    wb.save('articles.xlsx')
 
 def fetchAllFromTable(db, table):
     with sqlite3.connect(db) as con:
